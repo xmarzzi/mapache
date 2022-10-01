768d8a4050d8cbb0d8e14ef4291c5bd24248e9bf
Xavier Martinuzzi <xaviermagg@gmail.com>
2022-10-01 19:50:55 -0300

    Simplificando código con operadores ternarios

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
 const productosPet = [
-  {
-      id: 1,
-      title: "Cat chow Multiproteínas para Gatitos x3kg",
-      price:3200,
-      img: "https://hiperlibertad.vteximg.com.br/arquivos/ids/197744-1000-1000/ALIMENTO-PARA-GATOS-CAT-CHOW-GATITOS-PESCADO-CARNE-Y-VEGETALES-X-8-Kg-1-29005.jpg?v=637904938039600000",
-      
-  },
-  {
-      id: 2,
-      title: "Cat chow para gatos Esterilizados x3kg.",
-      price:2900,
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9KTYDxwvL83zjDRukQ0jnb8orbx_d_rJSlA&usqp=CAU3",
-      
-  },
-  {
-      id: 3,
-      title: "Dod chow razas minis x8kg.",
-      price:7800,
-      img: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20428697-p_oLGmuE.jpg",
-      
-  },
-  {
-      id: 4,
-      title: "Dog Chow Raza mediana y grande x21kg.",
-      price:19990,
-      img: "https://www.timberline.com.ar/5307-home_default/dog-chow-adulto-raza-mediana-y-grande-con-colorantes-x-21kg.jpg",
-      
-  },
-  {
-      id: 5,
-      title: "Dog chow cachorros x1.5kg.",
-      price:3250,
-      img: "https://www.lacoopeencasa.coop/media/lcec/publico/articulos/2/6/8/2688cd789af66df3cc744beda5a660eb",
-      
-  },
-
-  
-
+  { id: 1, title: "Cat chow Multiproteínas para Gatitos x3kg", price:3200, img: "https://hiperlibertad.vteximg.com.br/arquivos/ids/197744-1000-1000/ALIMENTO-PARA-GATOS-CAT-CHOW-GATITOS-PESCADO-CARNE-Y-VEGETALES-X-8-Kg-1-29005.jpg?v=637904938039600000", },
+  { id: 2, title: "Cat chow para gatos Esterilizados x3kg.", price:2900, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9KTYDxwvL83zjDRukQ0jnb8orbx_d_rJSlA&usqp=CAU3", },
+  { id: 3, title: "Dod chow razas minis x8kg.", price:7800, img: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20428697-p_oLGmuE.jpg", },
+  { id: 4, title: "Dog Chow Raza mediana y grande x21kg.", price:19990, img: "https://www.timberline.com.ar/5307-home_default/dog-chow-adulto-raza-mediana-y-grande-con-colorantes-x-21kg.jpg", },
+  { id: 5, title: "Dog chow cachorros x1.5kg.", price:3250, img: "https://www.lacoopeencasa.coop/media/lcec/publico/articulos/2/6/8/2688cd789af66df3cc744beda5a660eb", },
 ];
 
 let carrito = [];
@@ -44,9 +11,7 @@ let carrito = [];
 const items = document.querySelector("#items");
 const carritoHTML = document.querySelector("#carrito");
 
-
 //pintar productos en la tienda
-
 function renderizarProductos(){
   productosPet.forEach((producto) => {
       let productoHTML = `
@@ -82,13 +47,8 @@ function agregarProductoAlCarrito(id){
   let producto = productosPet.find((producto)=> producto.id === id);
   
   let productoEnCarrito = carrito.find((producto) => producto.id === id);
-  if(productoEnCarrito){
-      productoEnCarrito.cantidad++;
-  }else{
-      producto.cantidad = 1;
-      carrito.push(producto);
-  }
-  
+  productoEnCarrito ? productoEnCarrito.cantidad++ : (producto.cantidad = 1, carrito.push(producto));
+
   Swal.fire({
     position: 'center',
     icon: 'success',
@@ -134,21 +94,13 @@ const t = document.getElementById("total")
 t.innerHTML = `<h5>$${total}</h5>`
 }
 
-
-if (carritoStorage){
-    carrito = carritoStorage;
-    renderizarCarrito();
-    calcularTotal()
-}
+carritoStorage ? (carrito = carritoStorage, renderizarCarrito(), calcularTotal()) : ("");
 
 function eliminarProductoDelCarrito(id) {
 
     carrito[id].cantidad--;
+    carrito[id].cantidad === 0 ? carrito.splice(id, 1) : ("");
 
-    if (carrito[id].cantidad === 0) {
-        carrito.splice(id, 1);
-
-    }
     guardarCarrito();
     calcularTotal();
     renderizarCarrito();
@@ -165,17 +117,7 @@ function vaciarCarrito(){
     cancelButtonColor: '#3085d6',
     confirmButtonText: 'Sí, eliminar!'
   }).then((result) => {
-    if (result.isConfirmed) {
-        carrito = [];
-        renderizarCarrito();
-        calcularTotal();
-        localStorage.clear();
-      Swal.fire(
-        'Elimiando!',
-        'Tu carrito ha sido elimiando.',
-        'success'
-      )
-    }
+    result.isConfirmed ? (carrito = [], renderizarCarrito(), calcularTotal(), localStorage.clear(), Swal.fire('Elimiando!', 'Tu carrito ha sido elimiando.', 'success') ) : ("");
   })
   
 }
